8ed4594ef3fa0fba9151b54af2b89aaa423d45a2
Sathya Pramod K S <67020305+sathyapramodks@users.noreply.github.com>
2021-10-12 12:18:41 +0530

    Update settings.py

--- BytsLinksol/settings.py
+++ BytsLinksol/settings.py
@@ -123,7 +123,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+django_heroku.settings(locals())
